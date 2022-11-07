498d6228e4db08f79f51e95f3ce4a31f6d07cb3f
u31504 <u31504@P31504>
2022-11-07 11:00:30 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cherednichenko_Test_PR1_HW
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!!");
+        }
+
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye World!!");
+        }
+    }
+}
